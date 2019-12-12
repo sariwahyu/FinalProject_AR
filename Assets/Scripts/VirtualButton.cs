21d3cb5fc75e82da4629a953eddecaf6302c8b5a
sariwahyu <sariwahyuningsih22@gmail.com>
2019-12-12 20:02:10 +0700

    Markerless try 1

---
+++ Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+    private GameObject vbButtonObject;
+
+    void Start()
+    {
+        vbButtonObject = GameObject.Find("actionButton");
+        vbButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler(this);
+    }
+
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button pressed!");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    
+}
