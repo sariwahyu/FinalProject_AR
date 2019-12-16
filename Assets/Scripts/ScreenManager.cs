c787c04f929d574bca3a6a998d5f04e0681d8b39
sariwahyu <sariwahyuningsih22@gmail.com>
2019-12-16 10:36:00 +0700

    buat scene

---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenManager : MonoBehaviour
+{
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void SceneLoader(string SceneString)
+    {
+        SceneManager.LoadScene(SceneString);
+    }
+
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+
+}
